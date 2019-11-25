a82a4c9cc5fc17b602d75fbcdac81c5b0029fafc
KieranJeffreySmart <kieransmart77@googlemail.com>
2019-11-25 07:21:02 +0000

    Add project files.

---
+++ TestSFTP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestSFTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
